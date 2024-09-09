6b8e652d7492e5cda5ff4b4fdd8969f84150a447
JohnHovanec <jhovanec@towson.edu>
2024-09-09 14:05:15 -0400

    Add Config_Tab_Test table

--- database_scripts/db_creation_scripts.sql
+++ database_scripts/db_creation_scripts.sql
@@ -49,7 +49,7 @@ GO
 
 CREATE TABLE [epht].[Config_Theme_Test](
 -- Note that table columns are named to match the existing application object structure
-	configTheme_ID [int] IDENTITY(1,1) NOT NULL,
+	theme_ID [int] IDENTITY(1,1) NOT NULL,
 	topic_ID [int]  FOREIGN KEY REFERENCES  epht.Config_Topic_Test(topic_ID),  -- FK to Config_Topic_Test table
 	defaultTabPath [nvarchar](255) NULL,
 	themeTitle [nvarchar](255) NULL,
@@ -59,7 +59,7 @@ CREATE TABLE [epht].[Config_Theme_Test](
 	resources [nvarchar](max) NULL,
  CONSTRAINT [PK_Config_Theme_Test_ID] PRIMARY KEY CLUSTERED 
 (
-	[ConfigTheme_ID] ASC
+	[theme_ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
  CONSTRAINT [AK_Config_Theme_Test] UNIQUE NONCLUSTERED 
 (
@@ -69,3 +69,50 @@ CREATE TABLE [epht].[Config_Theme_Test](
 ) ON [PRIMARY]
 GO
 
+
+/*===================   epht].Config_Tab_Test   =====================*/		
+
+USE [MDHEPHT]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [epht].[Config_Tab_Test](
+-- Note that table columns are named to match the existing application object structure
+	tab_ID [int] IDENTITY(1,1) NOT NULL,
+	theme_ID [int]  FOREIGN KEY REFERENCES  epht.Config_Theme_Test(theme_ID),  -- FK to Config_Theme_Test table
+	tabTitle [nvarchar](255) NULL,
+	tabPath [nvarchar](255) NULL, 
+	contentType [nvarchar](50) NULL,
+	exportTitle [nvarchar](1000) NULL,
+	chartType [nvarchar](255) NULL,
+	selectable [bit] NULL,
+	baseline [nvarchar](255) NULL, 
+	defaultSelection [nvarchar](255) NULL, 
+	infoTitle [nvarchar](500) NULL,		-- was formerly part of the "info" object
+	infoId [nvarchar](500) NULL,		-- was formerly part of the "info" object
+	infoSubtitle [nvarchar](500) NULL,	-- was formerly part of the "info" object
+	chartTitle [nvarchar](500) NULL,
+	displayChartTitle [bit] NULL,
+	chartYAxisField [nvarchar](500) NULL,
+	displayChartDiscontinuityGraphic [bit] NULL,
+	displayXAxisLabel [bit] NULL,
+	xAxisLabel [nvarchar](500) NULL,
+	[url] [nvarchar](4000) NULL,
+	tableTitle [nvarchar](1000) NULL
+
+ CONSTRAINT [PK_Config_Tab_Test_ID] PRIMARY KEY CLUSTERED 
+(
+	[tab_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+ CONSTRAINT [AK_Config_Tab_Test] UNIQUE NONCLUSTERED 
+(
+	[tab_ID] ASC,
+	[tabPath] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
