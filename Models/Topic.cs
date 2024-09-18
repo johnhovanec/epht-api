73f238c0aedf8d1f092e957afa3e7488240f93f0
JohnHovanec <jhovanec@towson.edu>
2024-09-18 15:48:30 -0400

    Add Topics model

---
+++ Models/Topic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace epht_api.Models
+{
+    public class Topic
+    {
+        public int topic_ID { get; set; }
+        public string topicTitle { get; set; }
+        public string topicPath { get; set; }
+        public string category { get; set; }
+        public string defaultTabPath { get; set; }
+        public string overview { get; set; }
+        public string aboutData { get; set; }
+        public string countySuppressionRuleRange { get; set; }
+        public string countySuppressionRulePopMin { get; set; }
+        public string subCountySuppressionRuleRange { get; set; }
+        public string subCountySuppressionRulePopMin { get; set; }
+    }
+}
