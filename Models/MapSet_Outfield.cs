5193f6df0140257961e04d4c7dfdfede958120aa
JohnHovanec <jhovanec@towson.edu>
2024-10-25 16:22:33 -0400

    Remove fields from MapSet; add Outfield class

---
+++ Models/MapSet_Outfield.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace epht_api.Models
+{
+    public class MapSet_Outfield
+    {
+        [Key]
+        public int Outfield_ID { get; set; }
+        [ForeignKey("MapSet_ID")]
+        public int MapSet_ID { get; set; }
+        public string OutFieldName { get; set; }
+    }
+}
--- Models/Tab_MapSet.cs
+++ Models/Tab_MapSet.cs
@@ -14,28 +14,8 @@ namespace epht_api.Models
         public string Geometry { get; set; }
         public string TableTitle { get; set; }
 
-
-       
-        //public string Label { get; set; }
-        //public string SetName { get; set; }
-        //public bool Fill { get; set; }
-        //public byte Order { get; set; }
-        //public string YAxisID { get; set; }
-        //public string Type { get; set; }
-        //public byte PointRadius { get; set; }
-        //public byte PointBorderWidth { get; set; }
-        //public byte PointHoverRadius { get; set; }
-        //public byte PointHoverBorderWidth { get; set; }
-        //public byte LineTension { get; set; }
-        //public byte BorderWidth { get; set; }
-        //public string Stratification { get; set; }
-        //public string Title { get; set; }
-        //public bool Baseline { get; set; }
-        //public string DatasetType { get; set; }
-
-
         [NotMapped]
-        public List<string> Outfields { get; set; }
+        public List<MapSet_Outfield> Outfields { get; set; }
         [NotMapped]
         public List<Tab_ColumnHeader> ColumnHeaders { get; set; }
         [NotMapped]
