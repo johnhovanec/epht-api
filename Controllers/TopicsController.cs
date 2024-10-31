6a333cb5e9b9fbc36ee52db655afef913ca06459
John Hovanec <jhovanec@towson.edu>
2024-10-31 16:13:34 -0400

    Fix tabs iteration issue

--- Controllers/TopicsController.cs
+++ Controllers/TopicsController.cs
@@ -57,18 +57,18 @@ namespace epht_api.Controllers
             var themeQuery = _context.Config_Theme_Test.AsQueryable();
             topic.Themes = (List<Theme>)themeQuery.Where(x => x.Topic_ID == id).ToList();
 
-            // Create a list of tabs filtered by themes and inserts them into each theme
-            var tabQuery =
-                from tab in _context.Config_Tab_Test
-                join theme in _context.Config_Theme_Test on tab.Theme_ID equals theme.Theme_ID
-                where topic.Themes.Select(x => x.Theme_ID).Contains(tab.Theme_ID)
-                select tab;
-            List<Tab> tabs = tabQuery.ToList();
-
             // Loop through each theme and insert the related tabs
             for (int i = 0; i < topic.Themes.Count; i++)
             {
-                for (int j = 0; j < tabs.Count - 1; j++)
+                // Create a list of tabs for the given theme 
+                var tabQuery =
+                    from tab in _context.Config_Tab_Test
+                    join theme in _context.Config_Theme_Test on tab.Theme_ID equals theme.Theme_ID
+                    where tab.Theme_ID == topic.Themes[i].Theme_ID
+                    select tab;
+                List<Tab> tabs = tabQuery?.ToList();  
+
+                for (int j = 0; j < tabs.Count; j++)
                 {
                     if (topic.Themes[i].Theme_ID == tabs[j].Theme_ID)
                     {
@@ -84,21 +84,21 @@ namespace epht_api.Controllers
                         {
                             topic.Themes[i].Tabs[j].ChartConfigs = new List<Tab_ChartConfig>();
                         }
-                        topic.Themes[i].Tabs[j].ChartConfigs = _context.Config_Tab_ChartConfig_Test.Where(chartConfig => chartConfig.Tab_ID == tabs[j].Tab_ID).ToList();
+                        topic.Themes[i].Tabs[j].ChartConfigs = _context.Config_Tab_ChartConfig_Test.Where(chartConfig => chartConfig.Tab_ID == tabs[j].Tab_ID)?.ToList();
 
                         // Check if the List<UrlParams> exists and instantiate it if not
                         if (topic.Themes[i].Tabs[j].UrlParams == null)
                         {
                             topic.Themes[i].Tabs[j].UrlParams = new List<Tab_UrlParam>();
                         }
-                        topic.Themes[i].Tabs[j].UrlParams = _context.Config_Tab_UrlParam_Test.Where(urlParam => urlParam.Tab_ID == tabs[j].Tab_ID).ToList();
+                        topic.Themes[i].Tabs[j].UrlParams = _context.Config_Tab_UrlParam_Test.Where(urlParam => urlParam.Tab_ID == tabs[j].Tab_ID)?.ToList();
 
                         // Check if the List<MapSets> exists and instantiate it if not
                         if (topic.Themes[i].Tabs[j].MapSets == null)
                         {
                             topic.Themes[i].Tabs[j].MapSets = new List<Tab_MapSet>();
                         }
-                        topic.Themes[i].Tabs[j].MapSets = _context.Config_Tab_MapSet_Test.Where(mapSet => mapSet.Tab_ID == tabs[j].Tab_ID).ToList();
+                        topic.Themes[i].Tabs[j].MapSets = _context.Config_Tab_MapSet_Test.Where(mapSet => mapSet.Tab_ID == tabs[j].Tab_ID)?.ToList();
                     }
                 }
             }
